49e2d35c25145596e07a837d8b95fc93b51f0c29
Artem <gusev2014russia@mail.ru>
2019-09-28 19:09:20 +0300

    added work with web-sockets and added template for network page

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
-// const dns = require('dns');
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 const process = require('process');
-const path=require('path');
+const path = require('path');
 
 /**
  * @summary Pug settings
@@ -15,7 +16,7 @@ app.use(express.static(`${__dirname}/public`));
 /**
  * @summary data for main page
  */
-app.get('/', (req, res)=> {
+app.get('/', (req, res) => {
   const template = require('./lib/template');
   res.render('index', {
     title: 'Cheetah Software',
@@ -27,7 +28,7 @@ app.get('/', (req, res)=> {
 /**
  * @summary load data about your os
  */
-app.get('/os', async (req, res)=>{
+app.get('/os', async (req, res) => {
   try {
     const osInfo = require('./lib/os_info');
     const data = await osInfo.generalInfo();
@@ -44,37 +45,37 @@ app.get('/os', async (req, res)=>{
 /**
  * @summary load data about network
  */
-app.get('/network', async (req, res)=> {
+app.get('/network', async (req, res) => {
   try {
     const netInfo = require('./lib/net');
-    const netIface = await netInfo.netInterface();
-    setInterval(async function() {
-      const stats = await netInfo.getStats();
-      res.render('network', {
-        title: 'Network',
-        slogan: 'Get data about ip address, mask and etc.',
-        data: {...netIface, ...stats},
-      });
-    }, 5000);
+    const nsp = io.of('/network');
+    setInterval(async ()=>
+      nsp.emit('network_stats', await netInfo.getStats()), 1000);
+
+    res.render('network', {
+      title: 'Network',
+      slogan: 'Get data about ip address, mask and etc.',
+      data: {...await netInfo.netInterface(), ...await netInfo.getStats()},
+    });
   } catch (e) {
     console.error(e);
   }
 });
 
 
-app.get('/processor_info', (req, res)=>{
+app.get('/processor_info', (req, res) => {
   const {NUMBER_OF_PROCESSORS, PROCESSOR_ARCHITECTURE, PROCESSOR_IDENTIFIER} = process.env;
 });
 
-app.get('/memory_info', (req, res)=>{
+app.get('/memory_info', (req, res) => {
   const opTotalMem = os.totalmem();
   const opFreeMem = os.freemem();
 });
 
-app.get('/disks_info', (req, res)=>{
+app.get('/disks_info', (req, res) => {
   const disksInfo = require('./lib/disks');
 });
 
-app.listen(8000, ()=>{
+http.listen(8000, () => {
   console.log('Listen on http://127.0.0.1:8000');
 });
