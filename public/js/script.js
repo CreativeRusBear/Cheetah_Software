0dc9d7364a7f4b4639ff6b525eba14e97a0873a4
Artem <gusev2014russia@mail.ru>
2019-10-12 18:42:12 +0300

    added new section, create eslint config

--- public/js/script.js
+++ public/js/script.js
@@ -1,25 +1,25 @@
 /**
-                   ____     _____                        __
-                  /\  _`\  /\  __`\   /'\_/`\           /\ \__
-                  \ \ \/\ \\ \ \/\ \ /\      \          \ \ ,_\  _ __    __      __
-                   \ \ \ \ \\ \ \ \ \\ \ \__\ \   _______\ \ \/ /\`'__\/'__`\  /'__`\
-                    \ \ \_\ \\ \ \_\ \\ \ \_/\ \ /\______\\ \ \_\ \ \//\  __/ /\  __/
-                     \ \____/ \ \_____\\ \_\\ \_\\/______/ \ \__\\ \_\\ \____\\ \____\
-                      \/___/   \/_____/ \/_/ \/_/           \/__/ \/_/ \/____/ \/____/
-*/
+ *  ____     _____                        __
+ * /\  _`\  /\  __`\   /'\_/`\           /\ \__
+ * \ \ \/\ \\ \ \/\ \ /\      \          \ \ ,_\  _ __    __      __
+ *  \ \ \ \ \\ \ \ \ \\ \ \__\ \   _______\ \ \/ /\`'__\/'__`\  /'__`\
+ *   \ \ \_\ \\ \ \_\ \\ \ \_/\ \ /\______\\ \ \_\ \ \//\  __/ /\  __/
+ *    \ \____/ \ \_____\\ \_\\ \_\\/______/ \ \__\\ \_\\ \____\\ \____\
+ *     \/___/   \/_____/ \/_/ \/_/           \/__/ \/_/ \/____/ \/____/
+ */
 
 
 /**
  * @summary Нажатие на article
  */
-on('body', 'click', '.article', (e) => {
-  const {id} = e.path.find((elem)=>elem.className === 'article');
+on('body', 'click', '.article', e => {
+	const {id} = e.path.find(elem => elem.className === 'article');
 
-  /**
-   * @summary is last char === '/'
-   * @type {string}
-   */
-  window.location.href += /\/+$/.test(window.location.href) ? id : `/${id}`;
+	/**
+	 * @summary is last char === '/'
+	 * @type {string}
+	 */
+	window.location.href += /\/+$/.test(window.location.href) ? id : `/${id}`;
 });
 
 /**
@@ -29,20 +29,20 @@ on('body', 'click', '.article', (e) => {
  * @param fn
  * @private
  */
-function on(elSelector, eventName, selector, fn) {
-  const element = document.querySelector(elSelector);
+function on (elSelector, eventName, selector, fn) {
+	const element = document.querySelector(elSelector);
 
-  element.addEventListener(eventName, function(event) {
-    const possibleTargets = element.querySelectorAll(selector);
-    const target = event.target;
-    possibleTargets.forEach((item)=>{
-      let el = target;
-      while (el && el !== element) {
-        if (el === item) {
-          return fn.call(item, event);
-        }
-        el = el.parentNode;
-      }
-    });
-  });
+	element.addEventListener(eventName, function (event) {
+		const possibleTargets = element.querySelectorAll(selector);
+		const target = event.target;
+		possibleTargets.forEach(item => {
+			let el = target;
+			while (el && el !== element) {
+				if (el === item) {
+					return fn.call(item, event);
+				}
+				el = el.parentNode;
+			}
+		});
+	});
 }
